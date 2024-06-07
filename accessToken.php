bc17e2486656c1d91679c1d12e3dbf7679ec5366
kavil <paulkavilao1@gmail.com>
2024-06-08 02:06:15 +0300

    adding callback, and stk push

--- accessToken.php
+++ accessToken.php
@@ -10,7 +10,7 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($curl, CURLOPT_HEADER, FALSE);
 curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
 $result = curl_exec($curl);
-$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE); 
 $result = json_decode($result);
 echo $access_token = $result->access_token;
 curl_close($curl);
